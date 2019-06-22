3ed648c7bdb7f755ddf20b95515c91a0dc358fe0
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2019-06-23 00:54:14 +0200

    Fix #12 again

--- src/main/java/fr/arthurbambou/fblink/mixin/MixinChatMessageS2CPacket.java
+++ src/main/java/fr/arthurbambou/fblink/mixin/MixinChatMessageS2CPacket.java
@@ -16,6 +16,5 @@ public class MixinChatMessageS2CPacket {
     @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatMessageType;)V")
     public void MixinChatMessageS2CPacket(Component textComponent_1, ChatMessageType chatMessageType_1, CallbackInfo callbackInfo) {
         FBLink.getDiscordBot().sendMessage(textComponent_1.getString());
-        System.out.println(textComponent_1.toString());
     }
 }
