c11a478b07b60fb3a9bf7f88d989be235468ea85
yhr_C <keviny222333@gmail.com>
2019-12-16 01:11:54 -0600

    Various cleanup, preparing for looking more detailed into the message detection method. Trying to deal with special cases like the handling of /say command

--- src/main/java/fr/arthurbambou/fblink/discordstuff/DiscordBot.java
+++ src/main/java/fr/arthurbambou/fblink/discordstuff/DiscordBot.java
@@ -15,12 +15,9 @@ import net.minecraft.util.SystemUtil;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.ServerChannel;
-import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
-
-import java.util.NoSuchElementException;
 
 public class DiscordBot {
 
@@ -40,19 +37,19 @@ public class DiscordBot {
             return;
         }
 
-        if (token == "" || token.isEmpty()) {
+        if (token.isEmpty()) {
             System.out.println("[FDLink] Please add a bot token to the config file!");
             return;
         }
 
-        if (config.chatChannels.size() == 0) {
+        if (config.chatChannels.isEmpty()) {
             System.out.println("[FDLink] Please add a game chat channel to the config file!");
             this.hasChatChannels = false;
         } else {
             this.hasChatChannels = true;
         }
 
-        if (config.logChannels.size() == 0) {
+        if (config.logChannels.isEmpty()) {
             System.out.println("[FDLink] Please add a log channel to the config file!");
             this.hasLogChannels = false;
         } else {
@@ -60,6 +57,8 @@ public class DiscordBot {
         }
 
         if (!this.hasLogChannels && !this.hasChatChannels) return;
+
+        config.logChannels.removeIf(id -> config.chatChannels.contains(id));
 
         this.config = config;
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
@@ -73,166 +72,115 @@ public class DiscordBot {
         }));
         this.api = api;
 
-        if (this.hasChatChannels)
-            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(this.config.minecraftToDiscord.messages.serverStarting);
+        sendToAllChannels(this.config.minecraftToDiscord.messages.serverStarting);
 
-        if (this.hasLogChannels)
-            for (int a = 0; a < this.config.logChannels.size(); a++)
-                this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(this.config.minecraftToDiscord.messages.serverStarting);
-
-        ServerStartCallback.EVENT.register((minecraftServer1 -> {
-            try {
-                startTime = minecraftServer1.getServerStartTime();
-                if (this.hasChatChannels)
-                    for (int a = 0; a < this.config.chatChannels.size(); a++)
-                        this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(config.minecraftToDiscord.messages.serverStarted);
-
-                if (this.hasLogChannels)
-                    for (int a = 0; a < this.config.logChannels.size(); a++)
-                        this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(config.minecraftToDiscord.messages.serverStarted);
-
-            } catch (NoSuchElementException error) {
-                System.out.println(error);
-            }
+        ServerStartCallback.EVENT.register((server -> {
+            startTime = server.getServerStartTime();
+            sendToAllChannels(this.config.minecraftToDiscord.messages.serverStarted);
         }));
 
         ServerStopCallback.EVENT.register((server -> {
-            try {
-                if (this.hasChatChannels)
-                    for (int a = 0; a < this.config.chatChannels.size(); a++)
-                        this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(config.minecraftToDiscord.messages.serverStopped);
-
-                if (this.hasLogChannels)
-                    for (int a = 0; a < this.config.logChannels.size(); a++)
-                        this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(config.minecraftToDiscord.messages.serverStopped);
-
-
-                this.api.disconnect();
-            } catch (NoSuchElementException error) {
-                System.out.println(error);
-            }
+            sendToAllChannels(config.minecraftToDiscord.messages.serverStopped);
+            this.api.disconnect();
         }));
 
         ServerTickCallback.EVENT.register((server -> {
-            try {
-                int playerNumber = server.getPlayerManager().getPlayerList().size();
-                int maxPlayer = server.getPlayerManager().getMaxPlayerCount();
-                String ip = server.getServerIp();
-                int uptimeS = (int) (SystemUtil.getMeasuringTimeMs() - this.startTime) / 1000;
-                int uptimeM = uptimeS / 60;
-                int uptimeH = uptimeM / 60;
-                uptimeM = uptimeM - (uptimeH * 60);
-                uptimeS = uptimeS - (uptimeH * 60 * 60) - (uptimeM * 60);
-                if (this.hasReceivedaMessage) {
-                    if (this.messageCreateEvent.getMessageContent().startsWith("!playlist")) {
-                        String playerlist = "";
-                        for (PlayerEntity playerEntity : server.getPlayerManager().getPlayerList()) {
-                            playerlist = playerlist + playerEntity.getName().getString() + "\n";
-                        }
-                        if (playerlist.endsWith("\n")) {
-                            int a = playerlist.lastIndexOf("\n");
-                            playerlist = playerlist.substring(0, a);
-                        }
-                        this.messageCreateEvent.getChannel().sendMessage("Players : " + server.getPlayerManager().getPlayerList().size() + "/" + server.getPlayerManager().getMaxPlayerCount() + "\n\n" + playerlist);
+            int playerNumber = server.getPlayerManager().getPlayerList().size();
+            int maxPlayer = server.getPlayerManager().getMaxPlayerCount();
+            if (this.hasReceivedaMessage) {
+                if (this.messageCreateEvent.getMessageContent().startsWith("!playlist")) {
+                    StringBuilder playerlist = new StringBuilder();
+                    for (PlayerEntity playerEntity : server.getPlayerManager().getPlayerList()) {
+                        playerlist.append(playerEntity.getName().getString()).append("\n");
                     }
-                    this.lastMessageD = this.config.discordToMinecraft
-                            .replace("%player", this.messageCreateEvent.getMessageAuthor().getDisplayName())
-                            .replace("%message", EmojiParser.parseToAliases(this.messageCreateEvent.getMessageContent()));
-                    server.getPlayerManager().sendToAll(new LiteralText(this.lastMessageD));
+                    if (playerlist.toString().endsWith("\n")) {
+                        int a = playerlist.lastIndexOf("\n");
+                        playerlist = new StringBuilder(playerlist.substring(0, a));
+                    }
+                    this.messageCreateEvent.getChannel().sendMessage("Players : " + server.getPlayerManager().getPlayerList().size() + "/" + server.getPlayerManager().getMaxPlayerCount() + "\n\n" + playerlist);
+                }
+                this.lastMessageD = this.config.discordToMinecraft
+                        .replace("%player", this.messageCreateEvent.getMessageAuthor().getDisplayName())
+                        .replace("%message", EmojiParser.parseToAliases(this.messageCreateEvent.getMessageContent()));
+                server.getPlayerManager().sendToAll(new LiteralText(this.lastMessageD));
 
-                    this.hasReceivedaMessage = false;
+                this.hasReceivedaMessage = false;
+            }
+            if (this.hasChatChannels && this.config.minecraftToDiscord.booleans.customChannelDescription) {
+                int totalUptimeSeconds = (int) (SystemUtil.getMeasuringTimeMs() - this.startTime) / 1000;
+                final int uptimeH = totalUptimeSeconds / 3600 ;
+                final int uptimeM = (totalUptimeSeconds % 3600) / 60;
+                final int uptimeS = totalUptimeSeconds % 60;
+
+                for (String id : this.config.chatChannels) {
+                    this.api.getServerTextChannelById(id).ifPresent(channel ->
+                            channel.updateTopic(String.format(
+                            "player count : %d/%d,\n" +
+                                    "uptime : %d h %d min %d second",
+                            playerNumber, maxPlayer, uptimeH, uptimeM, uptimeS
+                    )));
                 }
-                if (this.hasChatChannels && this.config.minecraftToDiscord.booleans.customChannelDescription) {
-                    for (int a = 0; a < this.config.chatChannels.size(); a++) {
-                        ServerTextChannel channel = this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get();
-                        String topic =
-                                "player count : " + playerNumber + "/" + maxPlayer +
-                                ",\nuptime : " + uptimeH + " h " + uptimeM + 
-                                " min " + uptimeS + " second";
-                        channel.updateTopic(topic);
-                    }
-                }
-            } catch (NoSuchElementException error) {
-                System.out.println(error);
             }
         }));
     }
 
     public void sendMessage(Text text, MessageType messageType) {
         try {
-            try {
-                if (text.asString().equals(this.lastMessageD)) {
-                    return;
-                } else {
-                    if (MessageType.CHAT.equals(messageType) && this.config.minecraftToDiscord.booleans.PlayerMessages) {
-                        String string = text.asString();
-                        if (this.config.minecraftToDiscord.booleans.MCtoDiscordTag) {
-                            for (User user : this.api.getCachedUsers()) {
-                                ServerChannel serverChannel = (ServerChannel) this.api.getServerChannels().toArray()[0];
-                                Server server = serverChannel.getServer();
-                                string = string.replace(user.getName(), user.getMentionTag());
-                                string = string.replace(user.getDisplayName(server), user.getMentionTag());
-                                string = string.replace(user.getName().toLowerCase(), user.getMentionTag());
-                                string = string.replace(user.getDisplayName(server).toLowerCase(), user.getMentionTag());
-                            }
-                        }
-                        if (this.hasChatChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(EmojiParser.parseToUnicode(string));
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                if (!this.config.chatChannels.contains(this.config.logChannels.get(a))) {
-                                    this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(EmojiParser.parseToUnicode(string));
-                                }
-                    } else if (((TranslatableText) text).getKey().equals("multiplayer.player.left") && this.config.minecraftToDiscord.booleans.JoinAndLeftMessages) {
-                        if (this.hasChatChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(text.asString());
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                if (!this.config.chatChannels.contains(this.config.logChannels.get(a))) {
-                                    this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(text.asString());
-                                }
-                    } else if (((TranslatableText) text).getKey().startsWith("multiplayer.player.joined") && this.config.minecraftToDiscord.booleans.JoinAndLeftMessages) {
-                        if (this.hasChatChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(text.asString());
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                if (!this.config.chatChannels.contains(this.config.logChannels.get(a))) {
-                                    this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(text.asString());
-                                }
-                    } else if (((TranslatableText) text).getKey().startsWith("chat.type.advancement.") && this.config.minecraftToDiscord.booleans.AdvancementMessages) {
-                        if (this.hasChatChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(text.formatted(Formatting.RESET).asString());
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                if (!this.config.chatChannels.contains(this.config.logChannels.get(a))) {
-                                    this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(text.asString());
-                                }
-                    } else if (((TranslatableText) text).getKey().startsWith("commands.") && this.config.minecraftToDiscord.booleans.LogMessages) {
-                        if (this.hasChatChannels && !this.hasLogChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(text.asString());
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(text.asString());
-                    } else if (((TranslatableText) text).getKey().startsWith("death.") && this.config.minecraftToDiscord.booleans.DeathMessages) {
-                        if (this.hasChatChannels)
-                            for (int a = 0; a < this.config.chatChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.chatChannels.get(a)).get().sendMessage(text.asString());
-                        if (this.hasLogChannels)
-                            for (int a = 0; a < this.config.logChannels.size(); a++)
-                                this.api.getServerTextChannelById(this.config.logChannels.get(a)).get().sendMessage(text.asString());
+            if (text.asString().equals(this.lastMessageD)) {
+                return;
+            }
+
+            if (MessageType.CHAT.equals(messageType) && this.config.minecraftToDiscord.booleans.PlayerMessages) {
+                String string = text.asString();
+                if (this.config.minecraftToDiscord.booleans.MCtoDiscordTag) {
+                    for (User user : this.api.getCachedUsers()) {
+                        ServerChannel serverChannel = (ServerChannel) this.api.getServerChannels().toArray()[0];
+                        Server server = serverChannel.getServer();
+                        string = string.replace(user.getName(), user.getMentionTag());
+                        string = string.replace(user.getDisplayName(server), user.getMentionTag());
+                        string = string.replace(user.getName().toLowerCase(), user.getMentionTag());
+                        string = string.replace(user.getDisplayName(server).toLowerCase(), user.getMentionTag());
                     }
                 }
-            } catch (NullPointerException error) {
-                System.out.println(error);
+                sendToAllChannels(string);
+            } else if (((TranslatableText) text).getKey().equals("multiplayer.player.left") && this.config.minecraftToDiscord.booleans.JoinAndLeftMessages) {
+                sendToAllChannels(text.asString());
+            } else if (((TranslatableText) text).getKey().startsWith("multiplayer.player.joined") && this.config.minecraftToDiscord.booleans.JoinAndLeftMessages) {
+                sendToAllChannels(text.asString());
+            } else if (((TranslatableText) text).getKey().startsWith("chat.type.advancement.") && this.config.minecraftToDiscord.booleans.AdvancementMessages) {
+                sendToAllChannels(text.formatted(Formatting.RESET).asString());
+            } else if (((TranslatableText) text).getKey().startsWith("commands.") && this.config.minecraftToDiscord.booleans.LogMessages) {
+                if (this.hasLogChannels) {
+                    sendToLogChannels(text.asString());
+                } else {
+                    sendToChatChannels(text.asString());
+                }
+            } else if (((TranslatableText) text).getKey().startsWith("death.") && this.config.minecraftToDiscord.booleans.DeathMessages) {
+                sendToAllChannels(text.asString());
             }
-        } catch (NoSuchElementException error) {
-            System.out.println(error);
+        } catch (NullPointerException error) {
+            error.printStackTrace();
+        }
+    }
+
+    private void sendToAllChannels(String message) {
+        sendToChatChannels(message);
+        sendToLogChannels(message);
+    }
+
+    private void sendToLogChannels(String message) {
+        if (this.hasLogChannels) {
+            for (String id : this.config.logChannels) {
+                this.api.getServerTextChannelById(id).ifPresent(channel -> channel.sendMessage(message));
+            }
+        }
+    }
+
+    private void sendToChatChannels(String message) {
+        if (this.hasChatChannels) {
+            for (String id : this.config.chatChannels) {
+                this.api.getServerTextChannelById(id).ifPresent(channel -> channel.sendMessage(message));
+            }
         }
     }
 }
