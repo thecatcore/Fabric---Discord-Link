d6d88576d0bbdd58a06418fc8ef5a0baea0ed14b
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2020-08-01 19:11:23 +0200

    Force UTF_8 encoding, fixes #78

--- src/main/java/fr/arthurbambou/fdlink/FDLink.java
+++ src/main/java/fr/arthurbambou/fdlink/FDLink.java
@@ -8,10 +8,9 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -82,7 +81,7 @@ public class FDLink implements DedicatedServerModInitializer {
 		}
 
 		public String loadConfig() {
-			try (FileReader fileReader = new FileReader(configFile)) {
+			try (InputStreamReader fileReader = new InputStreamReader(new FileInputStream(configFile), StandardCharsets.UTF_8)) {
 				config = gson.fromJson(fileReader, Config.class);
 				if (config.token == null) {
 					config.token = DefaultConfig.token;
