df777c420732417546fb85ef3201cbcac8148541
Ev1dentFir3 <Ev1dentFir3@users.noreply.github.com>
2020-08-02 00:30:46 -0700

    Update FDLink.java

--- src/main/java/fr/arthurbambou/fdlink/FDLink.java
+++ src/main/java/fr/arthurbambou/fdlink/FDLink.java
@@ -189,6 +189,7 @@ public class FDLink implements DedicatedServerModInitializer {
 		}
 
 		public class MinecraftToDiscordBooleans {
+			public boolean playerName = true;
 			public boolean serverStartingMessage = true;
 			public boolean serverStartMessage = true;
 			public boolean serverStopMessage = true;
